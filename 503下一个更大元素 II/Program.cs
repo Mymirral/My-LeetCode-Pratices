132a4cb26b6073062aa09b521c41031ef6989919
Mirral <mirral114514@gmail.com>
2025-05-25 00:48:34 +0800

    做的新题目

---
+++ 503下一个更大元素 II/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+public class Solution
+{
+    static void Main(string[] args)
+    {
+        var res = NextGreaterElements([1, 2, 1]);
+        foreach (var item in res)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public static int[] NextGreaterElements(int[] nums)
+    {
+        var result = new int[2*nums.Length];
+        Array.Fill(result,-1);
+        var stack = new Stack<int>();
+
+        for(int i = 0; i<2*nums.Length; i++)
+        {
+            while (stack.Count > 0 && nums[stack.Peek() % nums.Length] < nums[i%nums.Length])
+            {
+                result[stack.Pop()] = nums[i % nums.Length];
+            }
+            stack.Push(i);
+        }
+
+        return result.Select(x => x == 0 ? -1 : x).Take(nums.Length).ToArray(); 
+    }
+}
---
+++ 901股票价格跨度/Program.cs
@@ -0,0 +1,42 @@
+﻿
+public class Test
+{
+    static void Main(string[] args)
+    {
+        StockSpanner stockSpanner = new StockSpanner();
+        Console.WriteLine(stockSpanner.Next(100));
+        Console.WriteLine(stockSpanner.Next(80));
+        Console.WriteLine(stockSpanner.Next(60));
+        Console.WriteLine(stockSpanner.Next(70));
+        Console.WriteLine(stockSpanner.Next(60));
+        Console.WriteLine(stockSpanner.Next(75));
+        Console.WriteLine(stockSpanner.Next(85));
+    }
+}
+public class StockSpanner
+{
+
+    List<int> list;
+    public StockSpanner()
+    {
+        list = new List<int>();
+    }
+
+    public int Next(int price)
+    {
+        list.Add(price);
+        int count = 1;
+        for (int i = list.Count - 1; i>=0; i--)
+        {
+            if (list[i] <= price) count++;
+            else break;
+        }
+        return count;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.Next(price);
+ */
