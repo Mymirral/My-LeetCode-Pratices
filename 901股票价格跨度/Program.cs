cd38e2a77d4ed0bddbc24ae5496cb988430eff24
Mirral <mirral114514@gmail.com>
2025-05-28 21:58:28 +0800

    用单调栈优化了题解

--- 901股票价格跨度/Program.cs
+++ 901股票价格跨度/Program.cs
@@ -15,15 +15,18 @@ public class Test
 }
 public class StockSpanner
 {
-
-    List<int> list;
+    int day;
+    Stack<Tuple<int,int>> stack;
     public StockSpanner()
     {
-        list = new List<int>();
+        day = -1;
+        stack = new Stack<Tuple<int,int>>();
+        stack.Push(new Tuple<int, int>(day,int.MaxValue));
     }
 
     public int Next(int price)
     {
+        /* 暴力解法
         list.Add(price);
         int count = 1;
         for (int i = list.Count - 1; i>=0; i--)
@@ -32,6 +35,18 @@ public class StockSpanner
             else break;
         }
         return count;
+        */
+        day++;
+        //首先这个题目,要找到是上一个更大的数，相当于放进栈里的，按照要求弹出来后，
+        //最后会呈现出单调递减的趋势。
+        //就是说，放进去的，如果比上一个数大，上一个数是要掉脑袋的。
+        while (stack.Count > 0 && price >= stack.Peek().Item2)
+        {
+            stack.Pop();
+        }
+        int res = day - stack.Peek().Item1;
+        stack.Push(new Tuple<int, int>(day, price));
+        return res;
     }
 }
 
