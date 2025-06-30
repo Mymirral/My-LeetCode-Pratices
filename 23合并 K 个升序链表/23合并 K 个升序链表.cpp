18720471e52e92e26fac470eea9295fdda5f8b46
Mirral <mirral114514@gmail.com>
2025-06-30 12:38:17 +0800

    日常做题

---
+++ 23合并 K 个升序链表/23合并 K 个升序链表.cpp
@@ -0,0 +1,54 @@
+﻿
+#include <vector>
+using namespace std;
+
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+
+
+
+class Solution {
+public:
+	ListNode* mergeKLists(vector<ListNode*>& lists) {
+		ListNode newlist(0);
+
+		ListNode* newhead = &newlist;
+		ListNode* cur = newhead;
+
+		while (cur)
+		{
+			int minindex = -1;
+			ListNode* minNode = NULL;
+			for (int i = 0; i < lists.size(); i++)
+			{
+				if (!lists[i]) continue;
+				if (!minNode)
+				{
+					minindex = i;
+					minNode = lists[i];
+				}
+				else
+				{
+					if (minNode->val > lists[i]->val)
+					{
+						minindex = i;
+						minNode = lists[i];
+					}
+				}
+			}
+			if (!minNode) break;  // 全为空，结束
+
+			lists[minindex] = lists[minindex]->next;
+			cur->next = minNode;
+			cur = cur->next;
+		}
+		return newhead->next;
+	}
+};
--- Q2相邻字符串之间的最长公共前缀/Program.cs
+++ Q2相邻字符串之间的最长公共前缀/Program.cs
@@ -1,36 +1,46 @@
 ﻿
 
+using System.Diagnostics.CodeAnalysis;
+
 public class Solution
 {
-
-    //竞赛超时了..
     public int[] LongestCommonPrefix(string[] words)
     {
-        int n = words.Length;
-        int[] lcp = new int[n - 1];
-        for (int i = 0; i < n - 1; i++)
-        {
-            lcp[i] = Presame(words[i], words[i + 1]);
-        }
+        int[] pre = new int[words.Length];
+        int[] sub = new int[words.Length];
+        int[] res = new int[words.Length];
 
-
-        int[] result = new int[words.Length];
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < words.Length; i++)
         {
             int max = 0;
-            if (i > 0)
-            max = Math.Max(max, i < n - 1 ? Presame(words[i - 1], words[i + 1]) : 0);
-
-            for (int j = 0; j < n - 1; j++)
+            for(int j = i+1; j< words.Length - i - 1; j++)
             {
-                if (j == i || j + 1 == i) continue;
-                max = Math.Max(max, lcp[j]);
+                int same = Presame(words[j], words[j + 1]);
+                max = Math.Max(max, same);
             }
-            result[i] = max;
+            pre[i] = max;
         }
-        return result;
-    }
 
+        for (int i = words.Length - 1; i >= 0; i--)
+        {
+            int max = 0;
+            for (int j = i-2;j >= 0; j--)
+            {
+                int same = Presame(words[j], words[j + 1]);
+                max = Math.Max(max, same);
+            }
+            sub[i] = max;
+        }
+
+        for(int i = 0;i < res.Length;i++)
+        {
+            if(i==0 || i== res.Length-1) res[i] = Math.Max(sub[i], pre[i]);
+            else res[i] = Math.Max(Math.Max(sub[i], pre[i]), Presame(words[i - 1], words[i+1]));
+        }
+
+        return res;
+
+    }
     public int Presame(string str1, string str2)
     {
         int same = 0;
@@ -57,6 +67,54 @@ public class Test
     {
         Solution solution = new Solution();
 
-        Console.WriteLine(solution.LongestCommonPrefix(["jump", "run", "run", "jump", "run"]));
+        Console.WriteLine(solution.LongestCommonPrefix(["efe", "feae", "fb"]));
     }
 }
+
+//竞赛超时了..
+/*public int[] LongestCommonPrefix(string[] words)
+{
+    int n = words.Length;
+    int[] lcp = new int[n - 1];
+    for (int i = 0; i < n - 1; i++)
+    {
+        lcp[i] = Presame(words[i], words[i + 1]);
+    }
+
+
+    int[] result = new int[words.Length];
+    for (int i = 0; i < n; i++)
+    {
+        int max = 0;
+        if (i > 0)
+        max = Math.Max(max, i < n - 1 ? Presame(words[i - 1], words[i + 1]) : 0);
+
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (j == i || j + 1 == i) continue;
+            max = Math.Max(max, lcp[j]);
+        }
+        result[i] = max;
+    }
+    return result;
+}
+
+public int Presame(string str1, string str2)
+{
+    int same = 0;
+    for (int i = 0; i < (str1.Length > str2.Length ? str2.Length : str1.Length); i++)
+    {
+        if (str1[i] == str2[i])
+        {
+            same++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return same;
+}*/
+
+
+//用前后缀去解决！
