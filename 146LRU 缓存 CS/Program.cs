c0be4274a5aa570808f61dc0102bb8f13fd3a03c
Mirral <mirral114514@gmail.com>
2025-07-19 21:11:53 +0800

    LRU缓存

---
+++ 146LRU 缓存 CS/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+public class LRUCache
+{
+    int _capacity;
+    Dictionary<int, LinkedListNode<(int, int)>> map = new();
+    LinkedList<(int, int)> cache = new();
+    public LRUCache(int capacity)
+    {
+        _capacity = capacity;
+    }
+
+    public int Get(int key)
+    {
+        if (map.ContainsKey(key))
+        {
+            cache.Remove(map[key]);
+            cache.AddFirst(map[key]);
+            return map[key].Value.Item2;
+        }
+        else return -1;
+    }
+
+    public void Put(int key, int value)
+    {
+        if (map.ContainsKey(key))   //已经有这个键值对了
+        {
+            var node = map[key]; //获取当前节点
+            cache.Remove(node); //从链表中移除
+        }
+        else if (map.Count >= _capacity)
+        {
+            //没有这个键值对，但是满了
+            map.Remove(cache.Last.Value.Item1);
+            cache.RemoveLast();
+        }
+        var newnode = new LinkedListNode<(int, int)>((key, value));
+        cache.AddFirst(newnode);
+        map[key] = newnode; //添加到字典中
+    }
+}
---
+++ 146LRU 缓存/146LRU 缓存.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+class LRUCache {
+public:
+    int _capacity;
+    unordered_map<int,int> map;
+    queue<int> cache;
+
+    LRUCache(int capacity) : _capacity(capacity)
+    {
+    };
+
+    int get(int key) {
+        if (map.find(key) != map.end())
+        {
+            int value = cache.back();
+            cache.pop();
+            cache.push(value);
+            return map[key];
+        }
+        else
+            return -1;
+    }
+
+    void put(int key, int value) {
+        if (map.find(key) != map.end())
+        {
+            map[key] = value;
+        }
+        else
+        {
+            if (cache.size() < _capacity)
+            {
+                map[key] = value;
+            }
+            else
+            {
+                cache.pop();
+                cache.push(key);
+                map[key] = value;
+            }
+        }
+    }
+};
