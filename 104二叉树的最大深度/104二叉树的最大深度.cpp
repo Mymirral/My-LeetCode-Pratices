da431f720e86be24ea72c94c78fcdc4609923993
Mirral <mirral114514@gmail.com>
2025-07-20 10:36:53 +0800

    二叉树

---
+++ 104二叉树的最大深度/104二叉树的最大深度.cpp
@@ -0,0 +1,38 @@
+﻿#include <queue>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+		queue<TreeNode*> q;
+		int floor = 0;
+
+		if(root) q.push(root);
+
+		while (!q.empty())
+		{
+			int loop = q.size();
+			for (int i = 0; i < loop; i++)
+			{
+				root = q.front();
+				q.pop();
+
+				if (root->left) q.push(root->left);
+				if (root->right) q.push(root->right);	
+			}
+			
+			floor++;
+		}
+
+		return floor;
+    }
+};
---
+++ 94二叉树的中序遍历/源.cpp
@@ -0,0 +1,34 @@
+
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	vector<int> res;
+	vector<int> inorderTraversal(TreeNode* root)
+	{
+		DFS(root);
+
+		return res;
+	}
+
+	void DFS(TreeNode* root)
+	{
+		if (root)
+		{
+			inorderTraversal(root->left);
+			res.push_back(root->val);
+			inorderTraversal(root->right);
+		}
+	}
+};
