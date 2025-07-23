73a5db55a4a9268369ee3c669462fcd346814cb5
Mirral <mirral114514@gmail.com>
2025-07-23 16:30:35 +0800

    二叉树、分治递归、BFS层序遍历

---
+++ 102二叉树的层序遍历/102二叉树的层序遍历.cpp
@@ -0,0 +1,51 @@
+﻿#include <vector>
+#include <queue>
+using namespace std; 
+
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+
+        queue<TreeNode*> queue;
+
+        vector<vector<int>> res; 
+        vector<int> floor;              //就用这一个来装每一层的数
+
+        queue.push(root);
+
+        //bfs
+        while (queue.size() > 0)
+        {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode* node = queue.front();
+                queue.pop();
+
+                if (node)   //当前节点不为空
+                {
+                    queue.push(node->left);
+                    queue.push(node->right);
+                    floor.push_back(node->val);
+                }
+            }
+
+            if(floor.size() > 0) res.push_back(floor);   //把这一层的数添加进去
+            floor.clear();  //清空这一层的
+        }
+
+        return res;
+    }
+};
---
+++ 108将有序数组转换为二叉搜索树/108将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+
+class Solution {
+public:
+	TreeNode* sortedArrayToBST(vector<int>& nums) {
+		TreeNode* node = new TreeNode;
+		node = toBST(nums, 0, nums.size() - 1);
+		return node;
+	}
+
+	TreeNode* toBST(vector<int>& nums, int left, int right)
+	{
+		TreeNode* node = new TreeNode;
+		int mid = left + (right-left)/2;
+		node->val = nums[mid];
+		if (mid - 1 >= left)
+			node->left = toBST(nums, left, mid - 1);
+		if (mid + 1 <= right)
+			node->right = toBST(nums, mid + 1, right);
+		return node;
+	}
+};
