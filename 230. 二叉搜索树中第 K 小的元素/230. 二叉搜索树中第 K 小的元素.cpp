fbce14454b7e24511191b4a901afc1892e0f7d34
Mirral <mirral114514@gmail.com>
2025-07-24 12:48:20 +0800

    二叉树搜索，平衡树

---
+++ 230. 二叉搜索树中第 K 小的元素/230. 二叉搜索树中第 K 小的元素.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+		int now = 1;
+		stack<TreeNode*> stack;		//用栈来回溯
+
+		while (!stack.empty() || root)	//栈不为空或者root不是空值，就循环
+		{
+			while (root)				//如果这个节点不是空的，就一直左子节点压进栈中
+			{
+				stack.push(root);	
+				root = root->left;
+			}
+
+			if (now == k) break;		//如果这个节点就是k，直接退出循环
+
+			root = stack.top();			//目前遍历的节点是栈顶
+			stack.pop();				
+			root = root->right;			//变成右节点再重复上面的过程
+
+			now++;
+		}
+
+		return stack.top()->val;
+    }
+};
---
+++ 98验证二叉搜索树/98验证二叉搜索树.cpp
@@ -0,0 +1,27 @@
+﻿#include <climits>
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+	
+};
+
+class Solution {
+public:
+	bool isValidBST(TreeNode* root) {
+		return validate(root, LONG_MAX, LONG_MIN);
+	}
+
+	bool validate(TreeNode* root,long max, long min)
+	{
+		if (!root) return true;
+
+		if (root->val >= max || root->val <= min) return false;
+
+		return validate(root->left, root->val, min) && validate(root->right, max, root->val);
+	}
+};
