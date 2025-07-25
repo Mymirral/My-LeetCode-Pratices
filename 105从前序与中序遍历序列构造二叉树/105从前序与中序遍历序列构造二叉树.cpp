6cdeea82d9c537b30fcb9c3fef8f07320301be8b
Mirral <mirral114514@gmail.com>
2025-07-25 11:43:40 +0800

    -二叉树-

---
+++ 105从前序与中序遍历序列构造二叉树/105从前序与中序遍历序列构造二叉树.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+		TreeNode* root = new TreeNode(preorder[0]);		//这棵树的根节点一定是先序遍历的根节点
+    }  
+
+	TreeNode* 
+};
