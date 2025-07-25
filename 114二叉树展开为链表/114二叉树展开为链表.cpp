f1b29c59d443e0e96b3b63500abafd2743f4caa8
Mirral <mirral114514@gmail.com>
2025-07-25 11:44:38 +0800

    -7.25- 二叉树- 层序遍历

--- 114二叉树展开为链表/114二叉树展开为链表.cpp
+++ 114二叉树展开为链表/114二叉树展开为链表.cpp
@@ -11,6 +11,27 @@
 class Solution {
 public:
 	void flatten(TreeNode* root) {
+		while (root)
+		{
+			TreeNode* right = root->right;	//记录右边的
 
+			//把左边接到右边，右边原来会断掉，所以上面记录了
+			if (root->left)
+			{
+				root->right = root->left;
+				root->left = nullptr;
+
+				//找到原来左节点的最右边
+				TreeNode* final = root->right;
+				while (final->right)
+				{
+					final = final->right;
+				}
+
+				final->right = right;
+			}
+
+			root = root->right;
+		}
 	}
 };
---
+++ 199二叉树的右视图/199二叉树的右视图.cpp
@@ -0,0 +1,41 @@
+﻿#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    
+};
+
+
+// ez pz
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> queue;
+        vector<int> res;
+
+        if (root)queue.push(root);
+        while (!queue.empty())
+        {
+            int size = queue.size();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode* node = queue.front();
+                queue.pop();
+                if (node->left) queue.push(node->left);
+                if (node->right) queue.push(node->right);
+
+                if (i == size - 1) res.push_back(node->val);
+            }
+        }
+
+        return res;
+    }
+};
