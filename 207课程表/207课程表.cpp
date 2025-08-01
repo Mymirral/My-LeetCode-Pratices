03c24576d791680993718ba46a6ddf12d848eba6
Mirral <mirral114514@gmail.com>
2025-08-01 12:57:16 +0800

    图论

---
+++ 207课程表/207课程表.cpp
@@ -0,0 +1,44 @@
+﻿#include <unordered_map>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        queue<int> queue;
+        unordered_map<int, vector<int>> coursenext; //课程 -> 后置课程
+        unordered_map<int, int> courseneednum;
+
+        for (int i = 0; i < prerequisites.size(); i++)
+        {
+            coursenext[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            courseneednum[prerequisites[i][0]]++;
+        }
+
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (courseneednum[i] == 0)  queue.push(i);
+        }
+
+        while (!queue.empty())
+        {
+            int course = queue.front();
+            queue.pop();
+
+            for (int i = 0; i < coursenext[course].size() ; i++)
+            {
+                courseneednum[coursenext[course][i]]--;
+                if (courseneednum[coursenext[course][i]] == 0) queue.push(coursenext[course][i]);
+            }
+        }
+
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (courseneednum[i] == 1)  return false;
+        }
+
+        return true;
+    }
+};
---
+++ 208实现 Trie (前缀树)/208实现 Trie (前缀树).cpp
@@ -0,0 +1,79 @@
+﻿#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Trie {
+
+    struct trienode
+    {
+        char letter;
+        bool end;
+        unordered_map<char, trienode*> next;
+
+        trienode() : letter(NULL), end(false){};
+        trienode(char c) : letter(c), end(false){};
+    };
+
+public:
+
+    trienode* head;
+
+    Trie() {
+        head = new trienode();
+    }
+
+    void insert(string word) {
+
+        trienode* node = head;
+
+        for (auto letter : word)
+        {
+            if (!node->next[letter])
+            {
+                node->next[letter] = new trienode(letter);
+            }
+            node = node->next[letter];
+        }
+
+        node->end = true;
+    }
+
+    bool search(string word) {
+        trienode* node = head;
+
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (!node->next[word[i]]) break;
+            node = node->next[word[i]];
+            if (i == word.size() - 1)
+            {
+                return node->end;
+            }
+        }
+        return false;
+    }
+
+    bool startsWith(string prefix) {
+        trienode* node = head;
+
+        for (int i = 0; i < prefix.size(); i++)
+        {
+            if (!node->next[prefix[i]]) break;
+            node = node->next[prefix[i]];
+            if (prefix.size() - 1 == i)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
