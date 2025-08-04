1ef043e00d207a8857bab4ed762f90f51b54e863
Mirral <mirral114514@gmail.com>
2025-08-04 12:10:10 +0800

    回溯

---
+++ 131分割回文串/131分割回文串.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+
+    vector<vector<string>> res;
+    vector<string> path;
+
+    vector<vector<string>> partition(string s) {
+
+    }
+
+    //切，然后遍历子串。 ← 
+    //是回文，加入队列，  |
+    //对接下来的子串， ---
+    void dfs(string& s, vector<bool>& used)
+    {
+
+    }
+
+    bool check(string s)
+    {
+        if (s.size() == 1) return true;
+
+        int start = 0;
+        int last = s.size() - 1;
+
+        while (start < last)
+        {
+            if (s[start] != s[last]) return false;
+            start++;
+            last--;
+        }
+
+        return true;
+    }
+};
