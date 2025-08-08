399a2fc48886cb50923f20c8149b57fd585cabc6
Mirral <mirral114514@gmail.com>
2025-08-08 14:16:32 +0800

    栈

---
+++ 20有效的括号/20有效的括号.cpp
@@ -0,0 +1,43 @@
+﻿#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> A;
+
+
+        for(auto c : s)
+        {
+            switch (c)
+            {
+            case '(': A.push(c); break;
+            case '[': A.push(c); break;
+            case '{': A.push(c); break;
+            case ')': 
+                if (A.empty())
+                {
+                    return false;
+                }
+                if (A.top() != '(') return false; else A.pop(); break;
+            case ']': 
+                if (A.empty())
+                {
+                    return false;
+                }
+                if (A.top() != '[')return false; else A.pop(); break;
+            case '}': 
+                if (A.empty())
+                {
+                    return false;
+                }
+                if (A.top() != '{')return false; else A.pop(); break;
+            default:
+                break;
+            }
+        }
+
+        return A.empty();
+    }
+};
