0d4ab60205de9ca3f36b47bf1e605e7744ebb9e2
Mirral <mirral114514@gmail.com>
2025-08-15 11:38:04 +0800

    堆/排序

---
+++ 215数组中的第K个最大元素/215数组中的第K个最大元素.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        return quickSelect(nums, 0, nums.size() - 1, k);
+    }
+
+    int quickSelect(vector<int>& nums, int left, int right, int k)
+    {
+        if (left == right) return nums[left];
+
+        int pivot = nums[left + rand()%(right-left + 1)];
+
+        int leftMax = left;
+        int rightMin = right;
+
+        int index = left;
+
+        while (index <= rightMin)
+        {
+            //当前小于基准
+            if (nums[index] < pivot)
+            {
+                swap(nums[leftMax], nums[index]);
+                index++;
+                leftMax++;
+            }
+            //当前大于基准
+            else if(nums[index] > pivot)
+            {
+                swap(nums[rightMin], nums[index]);
+                rightMin--;
+            }
+            else
+            {
+                index++;
+            }
+        }
+
+        int target = nums.size() - k;
+
+        if (target >= leftMax && target <= rightMin) return pivot;
+        else if (target < leftMax) return quickSelect(nums, left, leftMax-1,k);
+        else return quickSelect(nums, rightMin+1, right,k);
+    }
+};
---
+++ 347前 K 个高频元素/347前 K 个高频元素.cpp
@@ -0,0 +1,62 @@
+﻿#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+
+    unordered_map<int, int> map;
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        vector<int> freq;
+
+        for (auto num : nums)
+        {
+            map[num]++;
+        }
+
+        for (auto m : map)
+        {
+            freq.push_back(m.first);
+        }
+
+        quickSort(freq, 0, freq.size() - 1);
+
+        vector<int> res;
+        res.insert(res.end(), freq.begin(), freq.begin() + k);
+
+        return res;
+    }
+
+    void quickSort(vector<int>& nums, int left, int right)
+    {
+        if (left >= right) return;
+
+        int pivot = map[nums[right]];
+
+        int index = left;
+        
+        for (int i = left; i < right; i++)
+        {
+            if (pivot < map[nums[i]])
+            {
+                //降序
+                swap(nums[index], nums[i]);
+                index++;
+            }
+        }
+
+        swap(nums[right], nums[index]);
+
+        quickSort(nums, left, index - 1);
+        quickSort(nums, index + 1, right);
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int > t = { 1,1,1,2,2,3 };
+    s.topKFrequent(t,2);
+}
