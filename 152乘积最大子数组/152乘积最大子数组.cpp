bf899b4c1f9b61dade0cf2cbfedcc6f45ff8bb92
Mirral <mirral114514@gmail.com>
2025-08-22 13:33:15 +0800

    dp

---
+++ 152乘积最大子数组/152乘积最大子数组.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int maxProduct(vector<int>& nums) {
+
+		int len = nums.size();
+
+		int maxres = nums[0];
+
+		int maxdp = nums[0];
+		int mindp = nums[0];
+
+		for (int i = 1; i < len; i++)
+		{
+			int mx= maxdp,mi = mindp;
+			mindp = min(min(nums[i] * mi, nums[i]), nums[i] * mx);
+			maxdp = max(max(nums[i] * mx, nums[i]), nums[i] * mi);
+			maxres = max(maxdp, maxres);
+		}
+
+		return maxres;
+	}
+};
---
+++ 416分割等和子集/416分割等和子集.cpp
@@ -0,0 +1,45 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        //总和是偶数，就可以尝试分割，是奇数，返回false
+
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+        }
+
+        if (sum % 2) return false;
+
+        int target = sum / 2;
+
+        //目标,数组内能的和 能等于target
+        //找零钱..那道题的类似,但是数量有限
+        //494 目标和
+        vector<bool> dp(target+1,false);
+
+        dp[0] = true;
+        
+        
+        //背包问题 ， 有限背包
+        for (auto num : nums)
+        {
+            for (int i = target; i >= num ; i--) //倒序是有限背包
+            {
+                dp[i] = dp[i - num] || dp[i];
+            }
+        }
+
+        return dp[target];
+    }
+};
+
+int main()
+{
+    vector<int> x = { 3,3,6,8,16,16,16,18,20 };
+    Solution s;
+    s.canPartition(x);
+}
